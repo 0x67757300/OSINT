c4fc6945f53b2a9c739bd4d4afb03028c7717ccf
gus <0x67757300@gmail.com>
2023-12-20 18:49:29 -0300

    initial resources

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ const queryTypes = {
             },
             {
                 title: "Vazamentos",
-                link: "https://snusbase.com/"
+                link: () => "https://snusbase.com/"
             }
         ]
     },
@@ -46,6 +46,38 @@ const queryTypes = {
             {
                 title: "Google",
                 link: (q) => `https://www.google.com/search?q="${q}"`
+            },
+            {
+                title: "Facebook",
+                link: (q) => `https://www.facebook.com/${q}`
+            },
+            {
+                title: "Instagram",
+                link: (q) => `https://www.instagram.com/${q}`
+            },
+            {
+                title: "X (Twitter)",
+                link: (q) => `https://twitter.com/${q}`
+            },
+            {
+                title: "TikTok",
+                link: (q) => `https://www.tiktok.com/@${q}`
+            },
+            {
+                title: "Tinder",
+                link: (q) => `https://tinder.com/@${q}`
+            },
+            {
+                title: "Outros",
+                link: () => "https://whatsmyname.app/"
+            },
+            {
+                title: "Permutações",
+                link: () => "https://seintpl.github.io/NAMINT/"
+            },
+            {
+                title: "Vazamentos",
+                link: () => "https://snusbase.com/"
             }
         ]
     },
@@ -55,6 +87,18 @@ const queryTypes = {
             {
                 title: "Google",
                 link: (q) => `https://www.google.com/search?q="${q}"`
+            },
+            {
+                title: "Conta Google",
+                link: () => `https://github.com/mxrch/GHunt`
+            },
+            {
+                title: "Vazamentos (HIBP)",
+                link: () => "https://haveibeenpwned.com/"
+            },
+            {
+                title: "Vazamentos (Snusbase)",
+                link: () => "https://snusbase.com/"
             }
         ]
     },
@@ -66,7 +110,7 @@ const queryTypes = {
         resources: [
             {
                 title: "Google",
-                link: (q) => `https://www.google.com/search?q="${q}"`
+                link: (q, m) => `https://www.google.com/search?q="${m}" | "+55${q}" | "55${q}" | "${q}"`
             }
         ]
     },
@@ -109,7 +153,7 @@ const queryTypes = {
             {
                 title: "Google",
                 link: (q, m) => `https://www.google.com/search?q="${q}" | "${m}"`
-            }
+            },
         ]
     },
     domain: {
@@ -118,7 +162,35 @@ const queryTypes = {
             {
                 title: "Google",
                 link: (q) => `https://www.google.com/search?q=site:${q}`
-            }
+            },
+            {
+                title: "Registro.br",
+                link: (q) => `https://registro.br/tecnologia/ferramentas/whois?search=${q}`
+            },
+            {
+                title: "Whois",
+                link: (q) => `https://www.whoxy.com/${q}`
+            },
+            {
+                title: "Web Archive",
+                link: (q) => `https://web.archive.org/web/*/${q}`
+            },
+            {
+                title: "Google Cache",
+                link: (q) => `https://www.google.com/search?q=cache:${q}`
+            },
+            {
+                title: "Host",
+                link: (q) => `https://host.io/${q}`
+            },
+            {
+                title: "IP Reverso",
+                link: (q) => `https://viewdns.info/reverseip/?host=${q}`
+            },
+            {
+                title: "Registros DNS",
+                link: () => "https://hackertarget.com/dns-lookup/"
+            },
         ]
     },
     ip: {
@@ -127,10 +199,24 @@ const queryTypes = {
             {
                 title: "Google",
                 link: (q) => `https://www.google.com/search?q="${q}"`
+            },
+            {
+                title: "Dados e Localização",
+                link: (q) => `https://ipinfo.io/${q}`
+            },
+            {
+                title: "Pirataria",
+                link: (q) => `https://iknowwhatyoudownload.com/en/peer/?ip=${q}`
+            },
+            {
+                title: "Vazamentos",
+                link: () => "https://snusbase.com/"
             }
         ]
     }
 };
+
+
 const queryForm = document.querySelector("header form");
 const queryInput = document.querySelector("header form input");
 const querySelect = document.querySelector("header form select");
@@ -153,12 +239,12 @@ if (queryType && queryValue) {
         const h5 = document.createElement("h5");
         h5.innerHTML = resource.title;
         h5.className = "mb-0";
-        const link = (resource.link instanceof Function) ? resource.link(queryMask.unmaskedValue, queryMask.value) : resource.link;
+        const link = resource.link(queryMask.unmaskedValue, queryMask.value);
         const a = document.createElement("a");
         a.href = (new URL(link)).href;
         a.target = "_blank";
         a.innerText = link.replace(/^https?:\/\/(www\.)?/,"").replace(/\/$/, "").replaceAll(" ", "+");
-        a.className = "link-secondary stretched-link small";
+        a.className = "d-block text-truncate link-secondary stretched-link small";
         const div = document.createElement("div");
         div.className = "my-4 position-relative";
         div.appendChild(h5);
